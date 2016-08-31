22994c64b0c09f209d13fa5c29f5457c76ad935f
Domiii <dominik.seifert@gmail.com>
2016-08-31 18:14:55 +0800

    - more test code

---
+++ Assets/Scripts/Cat.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cat : MonoBehaviour {
+	public float Speed = 10;
+
+	void Update() {
+		var body = GetComponent<Rigidbody2D>();
+
+		body.velocity = Vector2.right * Input.GetAxis ("Horizontal") * Speed +
+						Vector2.up * Input.GetAxis ("Vertical") * Speed;
+	}
+		
+		
+
+
+	void OnMouseDown() {
+		GetComponent<Rigidbody2D> ().gravityScale = 1;
+		GetComponent<Rigidbody2D> ().velocity = Vector2.right * Speed;
+	}
+}

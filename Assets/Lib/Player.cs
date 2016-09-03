10b2d08565f9c1f1504f0561959e44296449027d
domi <dominik.seifert@gmail.com>
2016-09-03 18:18:22 +0800

    - minor update

---
+++ Assets/Lib/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float speed = 3;
+	public float jumpStrength = 5;
+	public bool isOnGround = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var body = GetComponent<Rigidbody2D> ();
+
+		var v = Vector2.right * Input.GetAxis ("Horizontal") * speed;
+		v.y = body.velocity.y;
+
+		if (isOnGround && Input.GetKeyDown(KeyCode.Space)) {
+			v.y = jumpStrength;
+		}
+
+		body.velocity = v;
+	}
+}
---
+++ Assets/Lib/PlayerFeet.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFeet : MonoBehaviour {
+	public Player Player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter() {
+		Player.
+	}
+}

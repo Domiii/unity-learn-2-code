52d3dc59809f87360e7bc0dd61ee7cdc49e7c94c
Domi <dominik.seifert@gmail.com>
2016-10-07 02:59:14 +0800

    added some encrypt stuff

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rock : MonoBehaviour {
+	public GameObject cat;
+	public float catSpeed;
+	public float catGravity;
+
+	void OnMouseDown() {
+		cat.GetComponent<Rigidbody2D> ().velocity = Vector2.right * catSpeed;
+		cat.GetComponent<Rigidbody2D> ().gravityScale = catGravity;
+	}
+}
--- Partial Setups/碰撞學徒/Assets/Scripts/Player.cs
+++ Partial Setups/碰撞學徒/Assets/Scripts/Player.cs
@@ -3,33 +3,28 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 	public float speed = 3;
-	public int collisionCount;
+	public float jumpStrength = 8;
+	public int groundCollisions;
 
 	void Update() {
+		// 用鍵盤來控制左右移動
 		var body = GetComponent<Rigidbody2D> ();
 		var v = body.velocity;
+
 		v.x = Input.GetAxis ("Horizontal") * speed;
+
+		if (Input.GetKeyDown (KeyCode.Space) && groundCollisions > 0) {
+			v.y = jumpStrength;
+		}
+
 		body.velocity = v;
 
 
+		// 跟者角色走的方向改看的方向
 		if (v.x != 0) {
 			var scale = transform.localScale;
 			scale.x = Mathf.Sign (v.x) * Mathf.Abs(transform.localScale.x);
 			transform.localScale = scale;
 		}
-	}
-
-	void OnCollisionEnter2D(Collision2D other) {
-		print ("Enter: " + other.gameObject.name);
-		++collisionCount;
-	}
-
-	void OnCollisionStay2D(Collision2D other) {
-		print ("Stay: " + other.gameObject.name);
-	}
-
-	void OnCollisionExit2D(Collision2D other) {
-		print ("Exit: " + other.gameObject.name);
-		--collisionCount;
 	}
 }
